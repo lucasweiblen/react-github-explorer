879b8b2fd86837f5553d804b1b213f7d0b57c2a9
Lucas Weiblen <lucasweiblen@gmail.com>
2019-04-25 21:29:08 -0300

    understanding mocks a little better

---
+++ src/__tests__/AddLanguageFormWithHooks.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import sinon from 'sinon';
+import Enzyme, {shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import AddLanguageFormWithHooks from '../components/AddLanguageFormWithHooks';
+
+Enzyme.configure({adapter: new Adapter()});
+
+describe('AddLanguageFormWithHooks', () => {
+  it('calls', () => {
+    const mockCallback = jest.fn();
+    const wrapper = shallow(
+      <AddLanguageFormWithHooks addLanguageHandler={mockCallback} />,
+    );
+    expect(wrapper.find('form')).toBeTruthy();
+    expect(wrapper.find('input[type="text"]')).toBeTruthy();
+    expect(wrapper.find('input[type="submit"]')).toBeTruthy();
+    expect(wrapper.props().addLanguageHandler).not.toBeNull();
+    const foo = wrapper.find('input[type="text"]');
+    foo.simulate('change', {target: {value: 'clojure'}});
+    wrapper.find('form').simulate('submit', {preventDefault: x => true});
+    expect(mockCallback.mock.calls.length).toBe(1);
+  });
+});
